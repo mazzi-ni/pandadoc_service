c36d871032f495b3a1cfb1984d4623fd0c3c2c4e
andreamazzini3 <a.mazzini3@campus.unimib.it>
2024-05-08 10:18:30 +0200

    add contract

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ContractModule } from './contract/contract.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ContractModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
